ada0e7e90d5c36c30d3981d363d6f1f2ad01e382
valerisvso <valerisvso@gmail.com>
2021-01-30 15:51:06 -0300

    characters components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './nprogress.css';
+import GlobalStyle from './global-styles'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Character from './character'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route path={`${process.env.PUBLIC_URL}/`} exact component={Character} />
+          <Route path={`${process.env.PUBLIC_URL}/:id`} exact component={Character} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/character.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import CharacterName from './character-name'
+import CharacterImage from './character-image'
+import CharacterDescription from './character-description'
+import CharacterPlaceholder from './character-placeholder'
+import Layout from './layout'
+import api from './api'
+import Next from './next'
+import CharacterContext from './character-context'
+
+
+const CharacterStyled = styled.div`
+`
+
+function Character({ match }) {
+  const [character, setCharacter] = useState({})
+  // el estado de cada character
+  useEffect(() => {
+    async function getCharacter() {
+        // vamos a obterner en primer personaje de la series
+      setCharacter(await api.getCharacter(match.params.id || 1))
+    }
+    getCharacter()
+  }, [match.params.id])
+  // se asignan los datos que vienen de la api para retornarlos
+
+  return (
+    <CharacterStyled>
+      <CharacterContext.Provider value={{
+        character,
+        setCharacter
+      }}>
+        <CharacterPlaceholder name={character.name} />
+        <Layout
+          next={<Next />}
+          name={<CharacterName name={character.name} />}
+          image={<CharacterImage image={character.image} name={character.name} />}
+          description={<CharacterDescription gender={character.gender} species={character.species} status={character.status} />}
+        />
+      </CharacterContext.Provider>
+    </CharacterStyled>
+  );
+}
+
+export default Character
